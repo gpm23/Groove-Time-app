bc76a9971b29dc541b2957632ea359eed01717c7
Gabriel Medeiros <gpm23@pitt.edu>
2023-07-18 21:29:26 -0400

    successfully configured spotify files

---
+++ app/src/main/java/com/example/groovetime/ViewPlaylistsActivity.java
@@ -0,0 +1,40 @@
+package com.example.groovetime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+
+
+import android.os.Bundle;
+
+
+public class ViewPlaylistsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_playlists);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // We will start writing our code here.
+    }
+
+    private void connected() {
+        // Then we will write some more code here.
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Aaand we will finish off here.
+    }
+}
